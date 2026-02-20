bd442c1aec5a9a04ab18dd98530ae945fb4cc02a
Nfishh <fixnafix1303@gmail.com>
2026-02-20 17:39:50 +0700

    Force re-add week1 files

---
+++ week1/3-Arithmetic.java
@@ -0,0 +1,12 @@
+/*
+Author : [Muhammad Nafis Idris]
+Class  : [1D]
+NIM    : [251524115]
+*/
+
+class FloatingPoint {
+    public static void main(String[] args) { double x = 92.98;
+int nx = (int) Math.round(x);
+System.out.println("x = " + x + ", nx = " + nx);
+         }
+}
